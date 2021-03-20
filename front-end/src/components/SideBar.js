1fde95cb5140d85c6b9965281c2cdd78cc4fd9f6
Sofia-Ortega <s.ortega@tamu.edu>
2021-03-20 12:18:28 -0500

    added sideBar component

---
+++ front-end/src/components/SideBar.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+export default function SideBar() {
+  const sidebar = {
+    
+  }
+
+  return (
+    <div style={sidebar}>
+      <p>Here's the sidebar</p>
+    </div>
+  )
+}
