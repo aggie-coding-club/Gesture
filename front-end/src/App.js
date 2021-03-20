4a3558a271baec93d747fc9bd126229ce432d336
Sofia-Ortega <s.ortega@tamu.edu>
2021-03-20 12:55:52 -0500

    added sidebar and cameraLayer side by side

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
 import SideBar from "./components/SideBar";
+import CameraScreen from "./components/CameraScreen";
 
 function App() {
+  const flexContainer = {
+    display: "flex",
+    backgroundColor: "blue"
+  }
+
+  const sideScreen = {
+    background: "grey",
+    width: "20vw",
+    height: "100vh"
+
+
+  }
+  const cameraScreen = {
+
+    background: "green",
+    width: "80vw"
+
+  }
   return (
-    <div>
-      <SideBar/>
+    <div style={flexContainer}>
+      <div style={sideScreen}>
+        <SideBar/>
+      </div>
+      <div style={cameraScreen}>
+        <CameraScreen />
+      </div>
     </div>
   );
 }
---
+++ front-end/src/components/CameraScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function CameraScreen() {
+
+  const camera = {
+
+  }
+
+  return (
+    <div style={camera}>
+      <p>Behold, the camera</p>
+    </div>
+  )
+}
