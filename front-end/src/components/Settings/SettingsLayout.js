69485b2157d1c7c3fe20a18db53ccc8a69bfe707
Sofia-Ortega <s.ortega@tamu.edu>
2021-04-17 10:41:48 -0500

    color covering entire screen

---
+++ front-end/src/components/Settings/SettingsLayout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import SideBar from "../MainScreen/SideBar";
+import CameraScreen from "../MainScreen/CameraScreen";
+
+export default function SettingsLayout() {
+  const settingsStyle = {
+    backgroundColor: "#090e18",
+    color: "#afb0b2",
+    height: "100vh",
+    padding: 0,
+    margin: 0
+  }
+
+  return (
+    <div style={settingsStyle}>
+      <p>Ello?</p>
+    </div>
+  )
+}
