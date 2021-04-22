c81b5cea4b3f7b15c7696e0fe2898da63db02849
hollow-owl <71401395+hollow-owl@users.noreply.github.com>
2021-04-22 14:42:35 -0500

    Add way to recognize series of gestures

--- recognition/Actions.py
+++ recognition/Actions.py
@@ -10,7 +10,6 @@ event = EventEmitter()
 # key down event, when the gesture is first made
 # :hand: left or right
 # :gest: the gesture
-
 @event.on("key down")
 def openProject(hand, gest):
     if hand == 'right' and gest == "Rock & Roll":
@@ -28,3 +27,66 @@ def testAction():
 @event.on("key down")
 def logGest(hand, gest):
     print(f"{hand} key down: {gest}")
+
+# gesture modes like mouse movement or quite mode
+##
+class MultiGesture():
+    def __init__(self, gestures, action, time_diff=1):
+        self.gestures = gestures
+        self.action = action
+        self.time_diff = time_diff
+        self.on = 0
+        self.time_last = None
+
+    def on_keydown(self, hand, gest):
+        # print(self.on)
+        if type(self.gestures[self.on]) == str:
+            if self.gestures[self.on] == gest: # doesn't matter which hand
+                # print("Go to next gesture")
+                self.on += 1
+            elif self.gestures[self.on] == "No Gesture":
+                return
+        elif hand == self.gestures[self.on][0] and gest == self.gestures[self.on][1]: # does check which hand
+            # print("Go to next gesture")
+            self.on += 1
+        elif self.gestures[self.on][0] == "No Gesture":
+            return
+        else:
+            self.on = 0
+
+        if self.on == len(self.gestures):
+            self.action()
+            self.on = 0
+
+# %%
+# m = MultiGesture(["1 finger", "2 finger"], lambda: print("counting"))
+# m.on_keydown("left", "1 finger")
+# m.on_keydown("right", "2 finger")
+
+# m.on_keydown("right", "2 finger")
+
+# m.on_keydown("left", "1 finger")
+# m.on_keydown("right", "Thumbs Down")
+# m.on_keydown("right", "2 finger")
+
+# m.on_keydown("left", "1 finger")
+# m.on_keydown("right", "2 finger")
+
+# %%
+# m = MultiGesture([("right", "1 finger"), "2 finger"], lambda: print("counting"))
+# m.on_keydown("left", "1 finger")
+# m.on_keydown("right", "2 finger")
+
+# m.on_keydown("right", "1 finger")
+# m.on_keydown("right", "2 finger")
+
+# %% 
+# m = MultiGesture([("right", "1 finger"), "2 finger"], lambda: print("counting"))
+# event.on("key down", m.on_keydown)
+
+# event.emit("key down", hand="right", gest="1 finger")
+# event.emit("key down", hand="right", gest="2 finger")
+
+# %%
+countToFive = MultiGesture(["Peace", "3 fingers", "4 fingers", "Open Hand"], lambda: print("Counted to 5"))
+event.on("key down", countToFive.on_keydown)
