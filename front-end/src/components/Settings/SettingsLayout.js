f2cc13feaa114c08204baf0fd0d81f8540a97203
Sofia-Ortega <s.ortega@tamu.edu>
2021-04-24 15:17:24 -0500

    clean up

--- front-end/src/components/Settings/SettingsLayout.js
+++ front-end/src/components/Settings/SettingsLayout.js
@@ -3,8 +3,6 @@ import TrickGesturesPairs from "./TrickGesturePairs";
 import configData from "../../data/config.json"
 
 export default function SettingsLayout() {
-  const tricks = ["open chrome", "mute volume", "raise volume", "lower volume"]
-  const defaultNum = [1, 2, 3, 4, 5, 6, 7, 8, 9] //FIXME: should be read from file
   const [data, setData] = useState(configData.settings);
 
 
@@ -41,10 +39,6 @@ export default function SettingsLayout() {
       </div>
       <div style={pairStyle}>
         <TrickGesturesPairs changeSettings={changeSettings} data={data}/>
-      </div>
-      <div>
-        <button onClick={changeSettings}>Change data</button>
-        <button onClick={displayData}>See data</button>
       </div>
     </div>
   )
