b0119f297081e03b1e4db4fc8c8dfbdae5c3a140
Sofia-Ortega <s.ortega@tamu.edu>
2021-06-12 14:35:53 -0500

    initial commit w boilerplate

---
+++ basic-electron-react-boilerplate/src/containers/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import '../assets/css/App.css';
+
+import HelloWorld from '../components/App/HelloWorld';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <HelloWorld />
+      </div>
+    );
+  }
+}
+
+export default App;
