9293f65dab5c2ee89af435c0bfb20d5b8f45caa1
Sofia-Ortega <s.ortega@tamu.edu>
2021-06-12 16:30:35 -0500

    cleaned up code and tested out class state

--- basic-electron-react-boilerplate/src/components/Main/Main.js
+++ basic-electron-react-boilerplate/src/components/Main/Main.js
@@ -1,13 +1,34 @@
 import MainLayout from "../MainScreen/MainLayout";
 import SettingsLayout from "../Settings/SettingsLayout";
-import React from 'react';
+import React, {Component} from 'react';
 import { Switch, Route } from 'react-router-dom';
+
+class Hiya extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+
+  handleClick() {
+    this.setState((prev) => ({counter: prev.counter+1}))
+  }
+
+  render() {
+    return(
+      <button onClick={this.handleClick}>{this.state.counter}</button>    )
+  }
+}
 
 export default function Main() {
     return (
-      <Switch> {/* The Switch decides which component to show based on the current URL.*/}
-        <Route exact path='/' component={MainLayout}></Route>
-        <Route exact path='/settings' component={SettingsLayout}></Route>
-      </Switch>
+      <Hiya />
+      // <Switch> {/* The Switch decides which component to show based on the current URL.*/}
+      //   <Route exact path='/' component={MainLayout}></Route>
+      //   <Route exact path='/settings' component={SettingsLayout}></Route>
+      // </Switch>
     );
   }
--- basic-electron-react-boilerplate/src/containers/App.js
+++ basic-electron-react-boilerplate/src/containers/App.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import Main from "../components/Main/Main"
-function Test() {
-  return (
-    <h1>Another test</h1>
-  )
-}
+
 
 function App() {
 
   return (
-
     <div>
-      <Test />
       <Main />
-
     </div>
   );
 
