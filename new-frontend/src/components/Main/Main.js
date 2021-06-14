0c3d7f1e31a3e3cebb234dca3f43ba8118d08313
dylansloann <dylansloann2@gmail.com>
2021-06-13 20:20:58 -0500

    added about back

--- new-frontend/src/components/Main/Main.js
+++ new-frontend/src/components/Main/Main.js
@@ -1,13 +1,15 @@
 import MainLayout from "../MainScreen/MainLayout";
 import SettingsLayout from "../Settings/SettingsLayout";
+import AboutLayout from "../About/AboutLayout";
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 
 export default function Main() {
     return (
-      //<MainLayout />
+      // <MainLayout />
       <SettingsLayout />
+      // <AboutLayout />
 
 
       //FIXME: ROUTES NOT WORKING
@@ -15,6 +17,7 @@ export default function Main() {
       // <Switch> {/* The Switch decides which component to show based on the current URL.*/}
       //   <Route exact path='/' component={MainLayout}></Route>
       //   <Route exact path='/settings' component={SettingsLayout}></Route>
+      //   <Route exact path='/about' component={AboutLayout}></Route>
       // </Switch>
     );
   }
