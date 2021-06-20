ecd16bd6482f7e5c8827dc5f8fab666cc80dbd59
Sofia-Ortega <s.ortega@tamu.edu>
2021-06-20 17:47:27 -0500

    path Type btn focus

--- frontend/src/components/Custom/CustomLayout.js
+++ frontend/src/components/Custom/CustomLayout.js
@@ -1,44 +1,111 @@
-import React from 'react';
+import React, {Component} from 'react';
 import MenuButtons from "../MainScreen/MenuButtons";
 
-export default function CustomLayout() {
-  const flexContainer = {
-    display: "flex",
-    margin: 0,
-    padding: 0,
-    backgroundColor: "#ececec",
-  };
+const activeBtn = {
+  backgroundColor: "#045bb7",
+  color: "white",
+}
 
-  const sideBar = {
-    flex: 1,
+const disabledBtn = {
+  backgroundColor: "#51595b",
+  color: "#bdb49d"
+}
+
+export default class CustomLayout extends Component{
+  constructor() {
+    super();
+
+    this.state = {
+      urlBtn: activeBtn,
+      fileBtn: disabledBtn
+
+    };
+    this.focusUrl = this.focusUrl.bind(this);
+    this.focusFile = this.focusFile.bind(this);
+
   }
-  const spacer = {
-    height: "10vh",
-    position: "relative",
-    top: "-4.5vh",
-  };
-  const mainContentStyle = {
 
-    margin: "0vh 1vh 1vh 0",
-    padding: 0,
-    flex: 3,
-    height: "100vh",
+  focusUrl() {
+    this.setState({ urlBtn: activeBtn });
+    this.setState({ fileBtn: disabledBtn });
+
+  }
+  focusFile() {
+    this.setState({ urlBtn: disabledBtn });
+    this.setState({ fileBtn: activeBtn });
 
   }
 
-  return (
-    <div style={flexContainer}>
-      <div style={mainContentStyle}>
-        <h2>On the right track</h2>
-      </div>
-      <div style={sideBar}>
-        <div style={spacer}></div>
-        <div>
-          <MenuButtons />
+  render() {
+
+
+    const flexContainer = {
+      display: "flex",
+      margin: 0,
+      padding: 0,
+      backgroundColor: "#ececec",
+    };
+
+    const sideBar = {
+      flex: 1,
+    }
+    const spacer = {
+      height: "10vh",
+      position: "relative",
+      top: "-4.5vh",
+    };
+
+    const mainContentStyle = {
+      margin: "0vh 1vh 1vh 0",
+      padding: 0,
+      flex: 3,
+      height: "100vh",
+    }
+
+    const titleStyle = {
+      textAlign: "center",
+      color: "#111111",
+      fontFamily: "Sacramento",
+      fontSize: 25,
+      margin: "8vh",
+    };
+
+    const pathTypeStyle = {
+
+    }
+
+    const pathTypeBtnStyle = {
+      border: "none",
+      outline: "none",
+      cursor: "pointer",
+      overflow: "hidden",
+      borderRadius: "10px",
+      height: "8vh",
+      padding: "0 10px",
+
+    }
+
+    return (
+      <div style={flexContainer}>
+        <div style={mainContentStyle}>
+          <div style={titleStyle}>
+            <h1>Customize</h1>
+          </div>
+
+          <div style={pathTypeStyle}>
+            <button style={Object.assign({}, pathTypeBtnStyle, this.state.urlBtn)} onClick={this.focusUrl}>Url</button>
+            <button style={Object.assign({}, pathTypeBtnStyle, this.state.fileBtn)} onClick={this.focusFile}>File</button>
+          </div>
+
+
         </div>
-
+        <div style={sideBar}>
+          <div style={spacer}></div>
+          <div>
+            <MenuButtons/>
+          </div>
+        </div>
       </div>
-
-    </div>
-  )
+    )
+  }
 }
